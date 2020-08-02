6d2b728ca4b25c24269d6a24896e947d70765479
kmaq07 <kqusalh484@gmail.com>
2020-08-02 21:16:52 +0400

    Updates

---
+++ precache-manifest.fe5104ee6e828cc9975b681ed54d0922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24dac1b1301a72b5db6c3c7068e98611",
+    "url": "/React-To-Do-List-Demo/index.html"
+  },
+  {
+    "revision": "3db9a41ff85653643e17",
+    "url": "/React-To-Do-List-Demo/static/css/main.da91ca52.chunk.css"
+  },
+  {
+    "revision": "feacb2a67329f77e99f9",
+    "url": "/React-To-Do-List-Demo/static/js/2.cd4d9b26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-Do-List-Demo/static/js/2.cd4d9b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3db9a41ff85653643e17",
+    "url": "/React-To-Do-List-Demo/static/js/main.aba9d36e.chunk.js"
+  },
+  {
+    "revision": "566c4eb66c2ce9f8f220",
+    "url": "/React-To-Do-List-Demo/static/js/runtime-main.5c867743.js"
+  }
+]);
